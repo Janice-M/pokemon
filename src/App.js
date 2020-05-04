66448320d38c66f1b47a0aecb2fa74422959c7f1
Janice Muia <janiceink001@gmail.com>
2020-05-04 23:48:00 +0300

    abys

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          jan banan<code>src/App</code> and save to reload.
+          jan bananc/App</code> and save to reload.
         </p>
         <a
           className="App-link"
