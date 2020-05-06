95dd87bbeb52103dc891114880f98b5249615c02
Janice Muia <janiceink001@gmail.com>
2020-05-06 23:06:01 +0300

    pokemon id and name maping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, {useState }from 'react';
 
 const pokemonsList = () => {
   const[pokemons]= useState([
-    id: 1, name : 'Bulba'
-  ])
+    {id: 1, name : 'Bulbasaur'},
+    {id:2, name : 'Charmander'},
+    {id:3 , name : 'squirtle'}
+
+  ]);
+
+
+  return (
+    <div className = 'pokemons-list'>
+      <h2>Pokemons List</h2>
+
+      {pokemons.map ((pokemon)=>
+        <div key={ '${pokemon.id}- ${pokemon.name}'} >
+        
+        
+        
+        
+        
+        </div> )}
+    </div>
+  )
 }
