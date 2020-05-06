d61ac400746de3d8e7fccbc41fbca01b45f049da
Janice Muia <janiceink001@gmail.com>
2020-05-06 23:11:08 +0300

    pokemonsList.js

---
+++ pokemonsList.js
@@ -0,0 +1,26 @@
+import React, {useState }from 'react';
+
+const pokemonsList = () => {
+  const[pokemons]= useState([
+    {id: 1, name : 'Bulbasaur'},
+    {id:2, name : 'Charmander'},
+    {id:3 , name : 'squirtle'}
+
+  ]);
+
+
+  return (
+    <div className = 'pokemons-list'>
+      <h2>Pokemons List</h2>
+
+      {pokemons.map ((pokemon)=>
+        <div key={ '${pokemon.id}- ${pokemon.name}'} >
+        <p>{pokemon.id}</p>
+        <p>{pokemon.name}</p>
+        
+        </div> )}
+    </div>
+  )
+}
+
+export default pokemonsList;
