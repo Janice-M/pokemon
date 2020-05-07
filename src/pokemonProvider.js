2ef221fc1e96063733e8776b17b212b417243ab6
Janice Muia <janiceink001@gmail.com>
2020-05-08 01:58:22 +0300

    my pokemons are back bois

--- src/pokemonProvider.js
+++ src/pokemonProvider.js
@@ -3,5 +3,10 @@ import React, { createContext, useState } from 'react';
 export const PokemonContext = createContext ();
 
 export const PokemonProvider = (props) => {
-    const [pokemons, ]
+    const [pokemons, setPokemons ] = useState ([
+        {id: 1, name : 'Bulbasaur'},
+        {id:2, name : 'Charmander'},
+        {id:3 , name : 'squirtle'}
+
+    ]);
 }
