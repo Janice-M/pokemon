5b426b34bcfed461421843fd16b89434bcb805fb
Janice Muia <janiceink001@gmail.com>
2020-05-09 22:42:19 +0300

    pokemon provider tag to also wrap in app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import PokemonList from './pokemonsList';
 import Pokedex from './capturedPokemon';
 
 const App = () =>(
+    <PokemonProvider>
     <div className = "App">
         <PokemonList  />
         <Pokedex   />
     </div>
+    </PokemonProvider>
 );
 
 export default App;
